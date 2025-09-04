7d37dda66af6f19de705129f9db8da91673d517f
dfntviu <dgomezp391@alumno.uaemex.mx>
2025-09-03 20:02:04 -0600

    Version mejorada para vista profesores chequen quien subió material

---
+++ src/Views/Notificaciones2Alumno.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const authStore = useAuthStore();
+    const notificaciones = ref([]);
+
+    const fetchNotificaciones = async () => {
+      if (!authStore.isAlumno) return;
+      notificaciones.value = await NotificacionService.getNotificacionesAlumno(authStore.userEmail);
+    };
+  
+</script>
